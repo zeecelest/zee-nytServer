8bbcd2fd4e0c674af936836a67df54fe5086a4ef
Zee Celest <zeecelest@gmail.com>
2019-10-29 18:50:28 -0400

    fixed syntax error

--- app.js
+++ app.js
@@ -8,28 +8,27 @@ app.use(morgan('common'));
 const books = require('./books-data.js');
 
 app.get('/books', (req, res) => {
-    //get search parameter and default it if its not provided
-    const { search = ' ', sort } = req.query;
+  //get search parameter and default it if its not provided
+  const { search = ' ', sort } = req.query;
 
-    //sort by title or rank
-    if (sort) {
-        if (!['title', 'rank'].includes(sort)) {
-            return res.status(400).send('Sort must be one of title or rank');
-        }
+  //sort by title or rank
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res.status(400).send('Sort must be one of title or rank');
     }
+  }
 
-    let results = books.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
+  let results = books.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
 
-    if (sort) {
-        results.sort((a, b) => {
-            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? 
-        
-        });
-    }
-    res.json(results);
+  if (sort) {
+    results = results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+  res.json(results);
 });
 
 
 app.listen(8000, () => {
-    console.log('Server started on PORT 8000');
+  console.log('Server started on PORT 8000');
 });
